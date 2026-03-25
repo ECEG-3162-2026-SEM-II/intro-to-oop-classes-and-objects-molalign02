9702adbce2390c821cf18fd35d3efabaa86e2696
molalign02 <ayizobelmolalign@gmail.com>
2026-03-26 02:03:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 
+public class Main {
+    public static void main(String[] args) {
 
+        // Create two student objects
+        Student student1 = new Student("John", 101);
+        Student student2 = new Student("Sara", 102);
+
+        // Print their names
+        System.out.println("Student 1: " + student1.getName());
+        System.out.println("Student 2: " + student2.getName());
+    }
+}
 
